d64f4336c6508c626f616c1c1e9faf757ff8bc7b
aarondupon <info@aarondupon.be>
2019-06-21 15:28:26 +0200

    update

---
+++ src/components/PlaylistPannel/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled, {css} from 'styled-components/primitives';
+
+
+const PlaylistPannel  = styled.View`
+    width:${props=>props.width};
+`;
+
+    
+export default PlaylistPannel;
